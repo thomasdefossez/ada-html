62dbc79dbdb1a2da2c5a9230fd03557075c1b7c7
thomasdefossez <tdefossez@gmail.com>
2025-06-07 17:50:10 +0200

    Mise à jour du projet - déploiement

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <h1>Hello React 👋</h1>
+            <p>Ton projet HTML est maintenant un projet React avec Vite !</p>
+        </div>
+    )
+}
+
+export default App
